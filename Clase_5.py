3571ae37cfa1573826a31a31a06379146e74e7bf
Kiino007 <kino007kevin@gmail.com>
2025-01-29 19:13:02 -0500

    clase_5
    Avance en la teoria de la clase

---
+++ Clase_5.py
@@ -0,0 +1,52 @@
+#Funciones de programacion
+
+
+#En esta sesion aprenderemos:
+
+#Que es una funcion 
+#Como se estructuran las funciones en un lenguaje de programacion
+#Cuales son los beneficios de utilizar funciones en nuestros programas
+#Como funcionan los parametros y el retorno de valores (Sin aplicarlo en codigo todavia)
+#Ejemplos y ejercicios practicos para comprender la logica detras del uso de funciones
+
+
+#Que es una funcion en programacion?
+#Una funcion es un bloque de codigo que tiene un proposito especifico dentro de un programa. se usa para 
+#dividir el codigo en partes mas pequeñas y reusables, lo que hace que los programas sean mucho mas 
+#organizados y faciles de enetender
+
+
+#Imagina que quieres preparar una pizza en casa. en lugar de hacer todo el proceso cada vez que la cocinas, 
+#podrias tener pasos organizados
+
+#preparar la masa
+#agregar los ingredientes
+#hornear la pizza
+#servir 
+
+#cada uno de estos pasos es una funcion dentro del proceso general de hacer una pizza. cuando quieras hacer 
+#otra pizza, simplemente sigues los mismos pasos en orden, sin necesidad de inventar un nuevo proceso desde cero
+
+
+#como funciona...
+
+#para entender como funciona una funcion, debemos pensar en 3 aspectos clave
+
+#1 Definicion de la funcion: Se establece el bloque de codigo con un nombre que lo identifica 
+
+#2 Invocacion de la funcion: Se ejecuta la funcion cuando es necesario dentro del programa 
+
+arreglo = []
+numero = len(arreglo)
+
+#Retorno de valores: Una funcion puede resolver un resultado para ser usado en alguna parte del codigo (return)
+
+#beneficios:
+#reusar codigo
+#En lugar de escribir el codigo varias veces, escribo eso una sola vez y la llamo cuando la necesite
+
+#Organizacion y modular
+#dividir un programa en funciones hace que el codigo sea mas estructurado y facil de entender 
+
+#3 Facilidad del mantenimiento
+#si hay errores en la ejecucion, se puden corregir en una sola funcion, en lugar de buscar varias partes del programa 
